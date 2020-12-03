3a82c1c30f2a90b7311e462808a4898501200bd2
Boone Epstein <booneepstein@gmail.com>
2020-12-03 15:05:02 -0600

    Update Game and Game-test to improve robustness.

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Round = require('../src/Round');
 
 describe('Game', () =>{
   beforeEach(() =>{
@@ -16,7 +17,20 @@ describe('Game', () =>{
     expect(game).to.be.an.instanceof(Game);
   });
 
-  // it('should be able to start', () => {
-  //   expect(game.start);
-  // });
+  it('should be able to start', () => {
+    expect(game).to.respondTo('start');
+  });
+
+  it('should set up when it starts', () => {
+    expect(game).to.respondTo('setUp');
+  });
+
+  it('should have a current round when it sets up', () => {
+    game.setUp();
+    expect(game.currentRound).to.be.instanceof(Round);
+  });
+
+  it('should not have a current round before setting up', () => {
+    expect(game.currentRound).to.equal(undefined);
+  });
 });
